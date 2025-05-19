452d7dfe0cc21cf9a65268f637e016c799beebae
Gagandeep Singh <gagan.official2k1@gmail.com>
2025-05-20 00:52:02 +0530

    adding next version of dnd todo

---
+++ components/DropComp.js
@@ -0,0 +1,71 @@
+"use client";
+
+import { DragDropContext } from "react-beautiful-dnd-next";
+// import "./App.css";
+import Column from "@/components/Column";
+import { useEffect, useState } from "react";
+import { cloneDeep } from "lodash";
+import data from "@/assets/data";
+
+const localData = localStorage.getItem("todos") ?? JSON.stringify(data);
+
+function DropComp() {
+  const [todos, setTodos] = useState(JSON.parse(localData));
+
+  const handleDragEnd = ({ source, destination, draggableId }) => {
+    console.log({ source, destination, draggableId });
+
+    // Checking if tasks dropped out and didn't landed over any columns OR dropped in same place inside same column then do nothing.
+    if (
+      !destination ||
+      (source.droppableId === destination.droppableId &&
+        source.index === destination.index)
+    ) {
+      console.log("if executed");
+      return;
+    }
+
+    // Deep cloning of todos array was necessary as nested arrays and objects were still
+    // mutable even after copying them using Array.from() method or spreading them [...]
+    const newData = cloneDeep(todos);
+
+    const destinationColumn = newData.find(
+      ({ id }) => id === destination.droppableId
+    );
+    const sourceColumn = newData.find(({ id }) => id === source.droppableId);
+
+    const [removedItem] = sourceColumn.tasks.splice(source.index, 1);
+    console.log({ removedItem });
+    destinationColumn.tasks.splice(destination.index, 0, removedItem);
+
+    setTodos(newData);
+    console.log({ k: 2, todos });
+  };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <section className="rounded-xl bg-white p-3 pb-8 my-auto">
+      <h1 className="text-lg font-semibold text-center mb-4 uppercase">
+        Todos Kanban
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <DragDropContext onDragEnd={handleDragEnd}>
+          {todos.map(({ id, column, tasks }) => (
+            <Column key={id} id={id} heading={column} tasks={tasks}></Column>
+          ))}
+        </DragDropContext>
+      </div>
+      <button
+        className="bg-[rgb(var(--redText))] text-[rgb(var(--redBg))] text-sm mt-4 mx-auto block px-3 py-2 rounded-md hover:opacity-80 active:scale-95 transition"
+        onClick={() => setTodos(data)}
+      >
+        Reset Local Storage
+      </button>
+    </section>
+  );
+}
+
+export default DropComp;
---
+++ components/TaskCard.js
@@ -0,0 +1,41 @@
+import { Draggable } from "react-beautiful-dnd-next";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { LuExternalLink } from "react-icons/lu";
+
+export default function TaskCard({ task, resolved, index }) {
+  const { id, title, date } = task;
+  const color = resolved ? "green" : "red";
+  const iconProps = { className: "inline" }; // Why I've used className this way? I was trying to inject same properties like classname, id etc to all the icons as needed
+
+  return (
+    <Draggable draggableId={id} key={id} index={index}>
+      {(provided, snap) => (
+        <li
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          style={{
+            "--bg": `rgba(var(--${color}Text),.07)`,
+            backdropFilter: snap.isDragging ? "blur(10px)" : "",
+            ...provided.draggableProps.style,
+          }}
+          className={`taskCard flex flex-col gap-1 rounded-md bg-[--bg] p-3 text-xs transition-colors`}
+        >
+          <a
+            style={{ "--text": `rgb(var(--${color}Text))` }}
+            className={`font-semibold text-sm w-fit hover:opacity-60 text-[--text]`}
+            href="https://siec-dashboard.netlify.app"
+            target="blank"
+          >
+            {title} <LuExternalLink {...iconProps} />
+          </a>
+          <span className="flex gap-1 items-center font-medium text-gray-500">
+            <FaRegCalendarAlt />
+            {date}
+          </span>
+          {provided.placeholder}
+        </li>
+      )}
+    </Draggable>
+  );
+}
